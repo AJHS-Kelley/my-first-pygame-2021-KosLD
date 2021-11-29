cd29efd52c31716f432cac0c97e7483ba8161fe1
Bruce S <70772398+KosLD@users.noreply.github.com>
2021-11-29 09:04:22 -0500

    Revert "v0.2"
    Updated color values

--- PyGamePractice.py
+++ PyGamePractice.py
@@ -1,11 +1,4 @@
-# PyGame Practice, Bruce Smith, 11/29/21 9:00am, v0.2
+# PyGame Practice, Bruce Smith, 11/29/21 8:55am, v0.1
 
 import pygame, sys
 from pygame.locals import *
-
-# start PyGame
-pygame.init()
-
-# Create game window
-windowSurface = pygame.display.set_mode((500, 400), 0, 32)
-pygame.display.set_caption("Hello, world!")
