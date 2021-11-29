8490f3f03c70ac2f4e462bb248bc3df476d8b350
Bruce S <70772398+KosLD@users.noreply.github.com>
2021-11-29 09:10:16 -0500

    v0.3
    Added color and font information.

--- PyGamePractice.py
+++ PyGamePractice.py
@@ -1,4 +1,4 @@
-# PyGame Practice, Bruce Smith, 11/29/21 9:04am, v0.2
+# PyGame Practice, Bruce Smith, 11/29/21 9:08am, v0.3
 
 import pygame, sys
 from pygame.locals import *
@@ -16,3 +16,9 @@ WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
+
+# Setup Fonts
+basicFont = pygame.font.SysFont(None, 48)
+
+# Setup Text
+text = basicFont.render('Hello, world', True, WHITE, BLUE)
